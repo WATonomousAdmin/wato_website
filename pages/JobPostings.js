8f702d42faf5abb83990dce1468d604436e759f4
kenzoengineer <kenzoengineer@gmail.com>
2023-04-14 00:35:48 -0400

    format initial commit

--- pages/JobPostings.js
+++ pages/JobPostings.js
@@ -15,40 +15,45 @@ export const getStaticProps = async () => {
 const JobPostings = ({allPostingsData}) => {
     console.log(allPostingsData);
     return (
-    <div className="overflow-x-hidden bg-zinc-800">
+    <div className="overflow-x-hidden font-bold">
         <div className="flex justify-left items-end w-screen h-screen bg-JobPostingHero">
-            <div className="font-sans text-6xl text-white m-32">
+            <div className="font-bold text-6xl text-white m-32">
                 Summer 2023 Applications
                 <br></br>
                 Are Now Open!
             </div>
         </div>
-        <div className="p-32 flex items-center">
-            <div className="h-96 w-[200em] relative">
-                <Image src={working} alt={"People Working"} layout="fill" objectFit="contain"></Image>  
-            </div>
-            <div className="flex flex-col text-white px-10">
-                <div className="font-medium text-6xl mb-10">
-                    Why Choose WATonomous?
+        <div className="bg-gradient-to-b from-zinc-900 to-zinc-700">
+            <div className="p-32 flex items-center">
+                <div className="h-96 w-[200em] relative">
+                    <Image src={working} alt={"People Working"} layout="fill" objectFit="contain"></Image>  
                 </div>
-                <div className="text-2xl">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in malesuada ex,
-                    at dignissim libero. Vestibulum imperdiet fermentum risus sit amet sodales. Nulla
-                    facilisi. Cras vitae scelerisque lacus. Quisque quis mauris placerat, posuere
-                    lacus eget, facilisis ligula. Sed id efficitur odio. Nullam lobortis massa
-                    vitae justo sagittis faucibus. Vestibulum ligula nisi, fermentum id nunc nec,
-                    maximus vehicula velit. Nam nec ullamcorper odio. Ut aliquet ipsum a mauris
-                    finibus cursus. Nulla iaculis velit enim, sit amet accumsan elit fermentum ac.
+                <div className="flex flex-col text-white px-10">
+                    <div className="font-bold text-6xl mb-10">
+                        Why Choose WATonomous?
+                    </div>
+                    <div className="font-medium text-2xl">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in malesuada ex,
+                        at dignissim libero. Vestibulum imperdiet fermentum risus sit amet sodales. Nulla
+                        facilisi. Cras vitae scelerisque lacus. Quisque quis mauris placerat, posuere
+                        lacus eget, facilisis ligula. Sed id efficitur odio. Nullam lobortis massa
+                        vitae justo sagittis faucibus. Vestibulum ligula nisi, fermentum id nunc nec,
+                        maximus vehicula velit. Nam nec ullamcorper odio. Ut aliquet ipsum a mauris
+                        finibus cursus. Nulla iaculis velit enim, sit amet accumsan elit fermentum ac.
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="mx-32 flex flex-col" style={{"overflow-anchor": "none"}}>
-            {/* <JobPostingList allPostingsData={allPostingsData}/> */}
-            <div className="text-white text-5xl text-center">
-                Open Roles
+            <div className="mx-32 flex flex-col" style={{"overflow-anchor": "none"}}>
+                {/* <JobPostingList allPostingsData={allPostingsData}/> */}
+                <div className="text-white text-5xl text-center mb-16">
+                    Open Roles
+                </div>
+                <JobPostingList
+                    data={allPostingsData}/>
             </div>
-            <JobPostingList
-                data={allPostingsData}/>
+            <div className="h-96">
+
+            </div>
         </div>
     </div>
     );
