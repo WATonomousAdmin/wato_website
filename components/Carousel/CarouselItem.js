bff3cf9de0461d911d34a0263a09ce523ee2d9d3
kenzoengineer <kenzoengineer@gmail.com>
2023-05-30 22:26:22 -0400

    idk why this wasn't working before

--- components/Carousel/CarouselItem.js
+++ components/Carousel/CarouselItem.js
@@ -14,8 +14,8 @@ const CarouselItem = ({ title, cta, blurb, body, image, selected, idx, onToggle 
     }
   }, [active]);
   return (
-    <div className={`m-5 h-[90vh] lg:h-[70vh] ${/*"group"*/ ""} ${!active && selected !== -1 ? "w-[0px] opacity-0 overflow-hidden" : "max-lg:w-[40vw] lg:w-[30%] delay-1000"}  transition-all duration-500`}>
-      <div className={`relative h-[64.25vh] lg:h-[50vh] transition-all ${active ? "-translate-x-1/2 delay-200 w-[200%]" : "w-[100%]"} duration-1000`}>
+    <div className={`m-5 h-[70vh] ${/*"group"*/ ""} ${!active && selected !== -1 ? "w-[0px] opacity-0 overflow-hidden" : "max-lg:w-[40vw] lg:w-[30%] delay-1000"}  transition-all duration-500`}>
+      <div className={`relative h-[50vh] transition-all ${active ? "-translate-x-1/2 delay-200 w-[200%]" : "w-[100%]"} duration-1000`}>
         <Image src={image} fill className="rounded-sm object-cover" />
       </div>
       <div id={idx} className={`duration-1000 ${active && "translate-x-full -translate-y-full z-50 delay-200 h-[71.4%]"} ${!active && selected !== -1 ? "text-transparent" : "text-black"} relative flex ${active ? "w-[95%] mx-[2.5%] shadow-2xl" : "w-[90%] mx-[5%] h-[40%] group-hover:h-[45%] group-hover:mx-[2.5%] group-hover:w-[95%] group-hover:shadow-2xl"} translate-y-[-50%] flex-col rounded-sm border-[0.5px] border-[#A6A6A6] bg-white px-4 py-6 transition-all justify-center`}>
