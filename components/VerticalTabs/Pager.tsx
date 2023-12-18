651ef8a0ccceada69606d927a3651c9e8660e890
Serena X <84420380+xuserena12@users.noreply.github.com>
2023-12-18 12:06:30 -0500

    fixed pager again

--- components/VerticalTabs/Pager.tsx
+++ components/VerticalTabs/Pager.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 interface PagerProps {
     identifier: string;
@@ -7,19 +7,8 @@ interface PagerProps {
     hidden?: boolean;
 }
 
-const Pager: React.FC<PagerProps> = ({ identifier, count, current, hidden }: PagerProps) => {
+const Pager = ({ identifier, count, current, hidden }: PagerProps) => {
     const [isDesktop, setDesktop] = useState(false);
-
-    useEffect(() => {
-        const updateMedia = () => {
-            setDesktop(window.innerWidth > 1200); // breakpoint
-        };
-
-        updateMedia();
-
-        window.addEventListener('resize', updateMedia);
-        return () => window.removeEventListener('resize', updateMedia);
-    }, []);
 
     const scrollTo = (idx: number) => {
         document
@@ -27,17 +16,43 @@ const Pager: React.FC<PagerProps> = ({ identifier, count, current, hidden }: Pag
             ?.scrollIntoView({ block: "center" });
     };
 
+    useEffect(() => {
+        if (window.innerWidth > 1200) {
+          setDesktop(true);
+        } else {
+          setDesktop(false);
+        }
+    
+        const updateMedia = () => {
+          if (window.innerWidth > 1200) {
+            setDesktop(true);
+          } else {
+            setDesktop(false);
+          }
+        };
+        window.addEventListener('resize', updateMedia);
+        return () => window.removeEventListener('resize', updateMedia);
+      }, []);
+    
+
     return (
-        <div className={`flex items-center transition-opacity duration-500 lg:flex-col ${hidden ? "opacity-0" : ""}`}>
+        <div
+            className={`flex items-center transition-opacity duration-500 lg:flex-col ${
+                hidden && "opacity-0"
+            }`}
+        >
             {[...Array(count)].map((_, i) => {
-                const dotClass = i === current ? (isDesktop ? "h-8" : "w-8") : (isDesktop ? "h-3" : "");
+                const dotSize = i === current ? (isDesktop ? "h-8" : "w-8") : (isDesktop ? "w-8" : "h-8")
 
                 return (
                     <div
                         key={i}
                         className={`m-4 h-3 w-3 cursor-pointer rounded-full transition-all duration-500 ${
-                            i === current ? "scale-125 transform bg-wato-blue" : "bg-white bg-opacity-50"
-                        } ${dotClass}`}
+                            i === current
+                                ? `scale-125 bg-wato-blue ${dotSize}`
+                                : "bg-white bg-opacity-50"
+                        }
+                        `}
                         onClick={() => {
                             scrollTo(i);
                         }}
