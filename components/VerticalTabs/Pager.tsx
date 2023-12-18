b16d3b36c692debe4b173e880b499891a5af4c70
Serena X <84420380+xuserena12@users.noreply.github.com>
2023-12-18 10:36:51 -0500

    pager is more dynamic now

--- components/VerticalTabs/Pager.tsx
+++ components/VerticalTabs/Pager.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 interface PagerProps {
     identifier: string;
     count: number;
@@ -5,7 +7,20 @@ interface PagerProps {
     hidden?: boolean;
 }
 
-const Pager = ({ identifier, count, current, hidden }: PagerProps) => {
+const Pager: React.FC<PagerProps> = ({ identifier, count, current, hidden }: PagerProps) => {
+    const [isDesktop, setDesktop] = useState(false);
+
+    useEffect(() => {
+        const updateMedia = () => {
+            setDesktop(window.innerWidth > 1200); // breakpoint
+        };
+
+        updateMedia();
+
+        window.addEventListener('resize', updateMedia);
+        return () => window.removeEventListener('resize', updateMedia);
+    }, []);
+
     const scrollTo = (idx: number) => {
         document
             .getElementById(`carousel-${identifier}-${idx}`)
@@ -13,20 +28,16 @@ const Pager = ({ identifier, count, current, hidden }: PagerProps) => {
     };
 
     return (
-        <div
-            className={`flex items-center transition-opacity duration-500 lg:flex-col ${
-                hidden && "opacity-0"
-            }`}
-        >
+        <div className={`flex items-center transition-opacity duration-500 lg:flex-col ${hidden ? "opacity-0" : ""}`}>
             {[...Array(count)].map((_, i) => {
+                const dotClass = i === current ? (isDesktop ? "h-8" : "w-8") : (isDesktop ? "h-3" : "");
+
                 return (
                     <div
                         key={i}
                         className={`m-4 h-3 w-3 cursor-pointer rounded-full transition-all duration-500 ${
-                            i === current
-                                ? "scale-125 transform h-8 bg-wato-blue"
-                                : "bg-white bg-opacity-50"
-                        }`}
+                            i === current ? "scale-125 transform bg-wato-blue" : "bg-white bg-opacity-50"
+                        } ${dotClass}`}
                         onClick={() => {
                             scrollTo(i);
                         }}
