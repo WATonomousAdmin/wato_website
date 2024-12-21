b10d45f9c927b70e9aea995a5c9332019f8e4e9f
kenzoengineer <kenzoengineer@gmail.com>
2024-12-20 21:26:33 -0800

    fix linting error

--- components/VerticalCards/VerticalCards.tsx
+++ components/VerticalCards/VerticalCards.tsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
 import Card from "./Card";
 import img01 from "../../public/imgs/img1.jpg";
-
-const SMALL_CONTENT_PLACEHOLDER =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor";
-const FULL_CONTENT_PLACEHOLDER =
-    "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
-
 const CardData = [
     {
         title: "800+ Alumni , 3 YC Founders",
