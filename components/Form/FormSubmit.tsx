b87e933bc892a7216bc83ac358db8432b5d57224
kenzoengineer <kenzoengineer@gmail.com>
2024-12-29 01:43:39 -0500

    fixed contact modal sizing, allowed for scroll

--- components/Form/FormSubmit.tsx
+++ components/Form/FormSubmit.tsx
@@ -2,7 +2,7 @@ const FormSubmit = () => {
     return (
         <div>
             <input
-                className="float-right flex cursor-pointer items-center rounded-md bg-wato-blue px-3 pb-2 pt-1 align-top font-medium text-zinc-100"
+                className="flex cursor-pointer items-center rounded-md bg-wato-blue px-3 pb-2 pt-1 align-top font-medium text-zinc-100 max-lg:mb-5 lg:float-right"
                 type={"submit"}
                 value={"Submit"}
             />
