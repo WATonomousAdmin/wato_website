ca30124824efd96edbb09239a4a64f0a23b4b295
kenzoengineer <kenzoengineer@gmail.com>
2025-04-16 01:13:28 -0400

    fonts accross the board smaller

--- components/ContentPane.tsx
+++ components/ContentPane.tsx
@@ -32,19 +32,19 @@ const ContentPane = ({
                     !leftOriented ? "lg:pr-44 xl:pr-60" : "lg:pl-44 xl:pl-60"
                 } relative z-10 flex flex-col justify-center text-white max-lg:items-center sm:px-16 lg:w-[51%]`}
             >
-                <div className="text-4xl font-black max-lg:text-center sm:text-5xl">
+                <div className="text-4xl font-black max-lg:text-center">
                     {title}&nbsp;<span className="text-wato-teal">{"//"}</span>
                 </div>
-                <div className="text-xl italic text-wato-teal max-lg:text-center lg:mb-6 lg:mt-2">
+                <div className="text-lg italic text-wato-teal max-lg:text-center lg:mb-6 lg:mt-2">
                     {subtitle}
                 </div>
-                <div className="max-lg:w-screen max-lg:p-12 max-lg:text-center">
+                <div className="max-lg:w-screen max-lg:px-12 max-lg:py-6 max-lg:text-sm">
                     {children}
                 </div>
             </div>
 
             <div
-                className={`flex w-4/5 justify-center self-center lg:w-[55%] ${
+                className={`flex w-4/5 justify-center self-center lg:w-[45%] ${
                     leftOriented ? "lg:pr-44 xl:pr-60" : "lg:pl-44 xl:pl-60"
                 } relative z-10`}
             >
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -21,6 +21,7 @@ const Navbar = ({ toggleModal, dark }: NavBarProps) => {
 
     return (
         <div className="flex w-screen justify-center">
+            <div className="pointer-events-none fixed z-30 h-screen w-screen bg-TopGradient" />
             <div className="align-center pointer-events-none fixed z-40 flex w-screen justify-between lg:mt-10 lg:w-[80vw] xl:max-w-[80rem]">
                 <NavbarBadge href={"/"}>
                     <Image src={WatoLogo} alt={"WATonomous logo"} width={150} />
